9b80f06dc926be4be77d7fba215a47bb85c9e08e
Luciana Ramos <ramos.d.luciana@gmail.com>
2023-12-17 22:27:32 -0300

    Atividade utilizando Collection Satck e correção de erros

--- src/estrutura_de_dados/CollectionQueueFila.java
+++ src/estrutura_de_dados/CollectionQueueFila.java
@@ -8,37 +8,59 @@ public class CollectionQueueFila {
     public static void main(String[] args) {
     	
         Queue<String> bankLine = new LinkedList<>();
-        Scanner scanner = new Scanner(System.in);
-        int opcao;
+        Scanner read = new Scanner(System.in);
+        int op;
 
-        while(opcao !=0 ) {
+        do {
+        	System.out.println("1: Adicionar um novo Cliente na fila");
+            System.out.println("2: Listar todos os Clientes na fila");
+            System.out.println("3: Chamar (retirar) uma pessoa da fila");
+            System.out.println("0: Finalizar o programa");
+            System.out.print("Escolha uma opção: ");
             
-            
-            System.out.println("\n\tMenu");
-            System.out.println("1: Adicionar Cliente");
-            System.out.println("2: Listar Clientes");
-            System.out.println("3: Chamar Cliente");
-            System.out.println("0: Sair");
+            op = read.nextInt();
 
-            switch (opcao) {
+            switch (op) {
                 case 1:
-                    System.out.println("Adicionar Cliente" + bankLine);
+                    System.out.println("Digite o nome do Cliente:");
+                    String clientName = read.nextLine();
+                    bankLine.add(clientName);
+                    System.out.println("Cliente adicionado com sucesso!");
                     break;
+                    
                 case 2:
-                	System.out.println("Listar Clientes" + bankLine);
+                	if (bankLine.isEmpty()) {
+                	System.out.println("A fila está vazia.");
+                	} else {
+                		System.out.println("Clientes na fila:");
+                		for (String cliente : bankLine) {
+                            System.out.println(cliente);
+                		}
+                        }
                     break;
                 case 3:
-                	System.out.println("Chamar Cliente" + bankLine);
-                    break;
+                	if (bankLine.isEmpty()) {
+                		System.out.println("A fila está vazia.");
+                	} else {
+                		String callClient = bankLine.poll();
+                		System.out.println("Cliente chamado: " + callClient);
+                	}
+            break;
+                    
                 case 0:
                     System.out.println("Programa finalizado.");
                     break;
+                    
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
+                
             }
+        } while (op != 0);
+
+        read.close();
 
         } 
     }
     
-}
+
 
--- src/estrutura_de_dados/CollectionStack.java
+++ src/estrutura_de_dados/CollectionStack.java
@@ -1,5 +1,65 @@
 package estrutura_de_dados;
 
-public class CollectionStack {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class CollectionStack {
+	
+	public static void main(String[] args) {
+		
+		Stack<String> bookStack = new Stack<String>();
+		Scanner read = new Scanner(System.in);
+		int i;
+		
+		do {
+            System.out.println("1: Adicionar um novo livro na pilha");
+            System.out.println("2: Listar todos os livros da Pilha");
+            System.out.println("3: Retirar um livro da pilha");
+            System.out.println("0: Finalizar o programa");
+            System.out.print("Escolha uma opção: ");
+
+            i = read.nextInt();
+            read.nextLine();
+            
+            switch (i) {
+            case 1:
+                System.out.print("Digite o nome do livro a ser adicionado: ");
+                String newBook = read.nextLine();
+                bookStack.push(newBook);
+                System.out.println("Livro adicionado com sucesso!");
+                break;
+
+            case 2:
+                if (bookStack.isEmpty()) {
+                    System.out.println("A pilha está vazia. Nenhum livro para listar.");
+                } else {
+                    System.out.println("Livros na pilha:");
+                    for (String livro : bookStack) {
+                        System.out.println(livro);
+                    }
+                }
+                break;
+
+            case 3:
+                if (bookStack.isEmpty()) {
+                    System.out.println("A pilha está vazia. Nenhum livro para retirar.");
+                } else {
+                    String livroRetirado = bookStack.pop();
+                    System.out.println("Livro retirado: " + livroRetirado);
+                }
+                break;
+
+            case 0:
+                System.out.println("Programa finalizado.");
+                break;
+                
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+                break;
+            }
+		} while (i != 0);
+
+        read.close();
+            
+	}
 }
